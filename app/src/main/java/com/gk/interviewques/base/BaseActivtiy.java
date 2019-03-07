b490d2eac72f46b3547f705fd9933e014185df8f
Hunny Goyal <goyal.hunny90@gmail.com>
2019-03-08 01:06:12 +0530

    SecondCommit

--- app/src/main/java/com/gk/interviewques/base/BaseActivtiy.java
+++ app/src/main/java/com/gk/interviewques/base/BaseActivtiy.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 public abstract class BaseActivtiy extends AppCompatActivity {
 
     public abstract int setLayout();
-//Comm
+//Commit
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
